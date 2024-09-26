c1e5f6d235ebfd62c429851933d008f34c3c0037
matheus <matheusrodrig16@gmail.com>
2024-09-26 20:28:19 -0300

    Refactor: temperature and prompt

--- infra/index_documents.py
+++ infra/index_documents.py
@@ -8,8 +8,10 @@ class Index:
         GENERATE_2_QUESTIONS_PROMPT = (
 
             'In all responses and interpretations, assume that the context is always related to programming, software development, or computer science.'
-            'Its only role is to generate 2 questions, in json format, based on the content of the uploaded files. '
+            'The questions must be related to programming, software development, or computer science, and should assess the students understanding of these topics based on the content learned'
+            'Its only role is to generate 2 questions, based on the content of the uploaded files. '
             'Both the values of the statement, the choices and reasons of the answers must be in Portuguese. '
+            'Make sure that both `question_1` and `question_2` follow the same structure with all fields properly filled, including the correct answer and reasoning.'
             'Instructions for the fields that must be in json:'
             '{'
                 '"question_1": {'
@@ -23,17 +25,7 @@ class Index:
                     '"correct_choice_index": "<fill_with_the_correct_letter>", '
                     '"reason": "<fill_with_why_the_answer_is_right>"'
                 '}, '
-                '"question_2": {'
-                    '"question_statement": "<fill_with_question_statement>", '
-                    '"choices": {'
-                        '"a": "<fill_with_choice_A_statement>", '
-                        '"b": "<fill_with_choice_B_statement>", '
-                        '"c": "<fill_with_choice_C_statement>", '
-                        '"d": "<fill_with_choice_D_statement>"'
-                    '}, '
-                    '"correct_choice_index": "<fill_with_the_correct_letter>", '
-                    '"reason": "<fill_with_why_the_answer_is_right>"'
-                '}'
+                '"question_2" : <fill_with_the_same_question_model>'
             '}'
         )
 
